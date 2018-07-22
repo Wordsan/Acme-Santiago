5ef966597552b5ae57cb5bb4215173b4b3aa2596
Wordsan <esagmo96@gmail.com>
2018-07-22 13:51:56 +0200

    Repos, services y pruebas done. Falla una query

--- src/main/java/repositories/RouteRepository.java
+++ src/main/java/repositories/RouteRepository.java
@@ -25,11 +25,11 @@ public interface RouteRepository extends JpaRepository<Route, Integer> {
 	Double[] routeLengthStadistics();
 
 	/* 3.3 -> Search for routes using a single key word that must appear somewhere in their names, their descriptions, or their hikes. */
-	@Query("select r from Route r join r.composedHikes h where r.name like '%?1%' or r.description like '%?1%' or  h.name like '%?1%' or h.originCity like '%?1%' or h.destinationCity like '%?1%'or h.description like '%?1%'")
+	@Query("select distinct r from Route r join r.composedHikes h where r.name like '%?1%' or r.description like '%?1%' or h.name like '%?1%' or h.originCity like '%?1%' or h.destinationCity like '%?1%'or h.description like '%?1%'")
 	List<Route> searchRoutesFromKeyWord(String keyWord);
 
 	/* 6.2 -> The outlier routes according to their lengths. */
-	@Query("select r from Route r where r.length <= (select avg(r.length) from Route r) - 3*(select stddev(r.length) from Route r) or r.length >= (select avg(r.length) from Route r) + 3*(select stddev(r.length) from Route r)")
+	@Query("select r from Route r where r.length <= ((select avg(r.length) from Route r) - 3*(select stddev(r.length) from Route r)) or r.length >= ((select avg(r.length) from Route r) + 3*(select stddev(r.length) from Route r))")
 	List<Route> outlierRoutes();
 
 	/* 3.4 -> Search for routes whose length is in a user-provided range. */
@@ -37,11 +37,10 @@ public interface RouteRepository extends JpaRepository<Route, Integer> {
 	List<Route> routesByLengthRange(Double l1, Double l2);
 
 	/* 16.6 -> The average number of comments per route (including their hikes). */
-	@Query("select avg(r.comments.size + sum(h.comments)) from Route r join r.composedHikes h")
+	@Query("select avg(r.comments.size + h.comments.size) from Route r join r.composedHikes h")
 	Double avgCommentsPerRoute();
 
-	//TODO
 	/* 3.5 -> Search for routes that have a minimum or a maximum number of hikes. */
-	@Query("")
+	@Query("select r from Route r where r.composedHikes.size = (select min(r.composedHikes.size) from Route r) or r.composedHikes.size = (select max(r.composedHikes.size) from Route r)")
 	List<Route> routesByHikesSize();
 }
